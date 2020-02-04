149fadc3a9e40e0611e7b1382c45b333067d28d0
abkhuddus <abjava121@gmail.com>
2020-02-04 16:02:34 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,5 +17,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  pageTitle = 'Ab Product Management';
+  pageTitle = 'Ab Products Management';
 }
